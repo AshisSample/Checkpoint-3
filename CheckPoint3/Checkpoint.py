466e0870508fd888e5f4bb64a03d191716073ee3
AshisSample <101118761+AshisSample@users.noreply.github.com>
2022-04-27 14:10:43 -0500

    Add files via upload

---
+++ CheckPoint3/Checkpoint.py
@@ -0,0 +1,22 @@
+mystring="Hello World"
+mylist= list(range(10))
+mybool=1>2
+
+mystringindex=mystring[0:3]
+
+mylistindex=mylist[0]
+
+newNumber=mylistindex + 10
+
+mylistindexlast=mylist[9]
+
+names = 'harry,alex,susie,jared,gail,conner'
+namelist=names.split(",")
+
+Upperindex=mystring[0:6].upper()
+newstringup=Upperindex + mystring[6:11]
+mynumber= 185
+
+stringinterp=f'my number is {mynumber}'
+
+print(mystring)
